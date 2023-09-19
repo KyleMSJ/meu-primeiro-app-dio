17cfee55f3d0e5fb5c47eeb99f81d04168450ec0
KyleMSJ <caiomarcelosj3@gmail.com>
2023-09-19 01:09:18 -0300

    add internacionalizacao em 8 idiomas

---
+++ app/src/main/java/br/com/dio/meuprimeiroappdio/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.dio.meuprimeiroappdio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.igorbag.meuprimeiroappdio.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
